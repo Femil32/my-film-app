bfb1c0767d62b5676ff94de72d543715563282d0
Femil Savaliya <femilsavaliya555@gmail.com>
2023-03-14 07:08:32 +0530

    add svg with index file

---
+++ client/src/assets/img/index.jsx
@@ -0,0 +1,141 @@
+import CameraImage from './base/camera.png'
+import FilmReel from './base/film-reel.png'
+import TransparentProfilePicture from './profile/bg-profile.png'
+import LogoIcon from './icon/logo-icon.svg'
+import OfferIcon from './icon/offer.svg'
+import StarIcon from './icon/star-y.svg'
+import LocationIcon from './icon/location.svg'
+import LeftArrowIcon from './icon/left-arrow-b.svg'
+import CameraIcon from './icon/camera.svg'
+import FilterIcon from './icon/filter.svg'
+import SortIcon from './icon/sort.svg'
+import SinemaIcon from './base/sinema.svg'
+import LogoOne from './logo/logo-32x32.svg'
+import LogoTwo from './logo/logo-64x64.svg'
+import LogoThree from './logo/logo-128x128.svg'
+import DeleteIcon from './icon/delete-o.svg'
+import DownArrowIcon from './icon/downarrow.svg'
+import EditIcon from './icon/edit-navy-b.svg'
+import EyeIcon from './icon/esy-s-db.svg'
+import InfoIcon from './icon/info-red.svg'
+import InstagramIcon from './icon/instagram.svg'
+import LinkedInIcon from './icon/linkedIn.svg'
+import MenuIcon from './icon/manu.svg'
+import NotificationIcon from './icon/notification-ball.svg'
+import PlusBIcon from './icon/plus-b.svg'
+import RightDarkBIcon from './icon/right-dark-b.svg'
+import RightDarkBlueIcon from './icon/right-dark-blue.svg'
+import SearchIcon from './icon/search.svg'
+import TwitterIcon from './icon/twitter.svg'
+import ShortIcon from './icon/sort.svg'
+import StarOIcon from './icon/star.svg'
+import SuccessfulIcon from './icon/successful.svg'
+import UserIcon from './icon/user.svg'
+import ImageOne from './profile/img-1.png'
+import ImageTwo from './profile/img-2.png'
+import ImageThree from './profile/img-3.png'
+import ImageFour from './profile/img-4.png'
+import ImageFive from './profile/img-5.png'
+import ImageSix from './profile/img-6.png'
+import ImageSeven from './profile/img-7.png'
+import ImageEight from './profile/img-8.png'
+import ImageNine from './profile/img-9.png'
+import ImageTen from './profile/img-10.png'
+import ImageEleven from './profile/img-11.png'
+import ProfilePictureOne from './profile/p-1.png'
+import ProfilePictureTwo from './profile/profile.png'
+import ProfilePictureThree from './profile/profile1.png'
+import ProfilePictureFour from './profile/profile2.png'
+import ProfilePictureFive from './profile/profile3.png'
+import ProfilePictureSix from './profile/profile4.png'
+import ProfilePictureSeven from './profile/profile5.png'
+import ProfilePictureEight from './base/img1.png'
+import Cameraman from './base/camera-man.png'
+import CameraProfile from './base/camera-profile.png'
+import ImgLp from './base/img-lp.png'
+import ImageTwelve from './profile/img-12.png'
+import CameraMan from './base/cameraman.png'
+import Producation from './base/Producation.png'
+import HandCamera from './base/HandCamera.png'
+import location from './base/location.png'
+import MemberCameraman from './base/Member-Cameraman.png'
+import Actor from './base/Actor.png'
+import Travel from './base/Travel.png'
+import Singer from './base/Singer.png'
+import CrewMembers from './base/CrewMembers.svg'
+import ServiceProvider from './base/ServiceProvider.svg'
+import naomi from './base/naomi.png'
+import van from './base/van.png'
+import van1 from './base/van1.png'
+
+export {
+    CameraImage,
+    FilmReel,
+    LogoIcon,
+    OfferIcon,
+    StarIcon,
+    LocationIcon,
+    LeftArrowIcon,
+    FilterIcon,
+    SortIcon,
+    CameraIcon,
+    SinemaIcon,
+    LogoOne,
+    LogoTwo,
+    LogoThree,
+    TransparentProfilePicture,
+    DeleteIcon,
+    DownArrowIcon,
+    EditIcon,
+    EyeIcon,
+    TwitterIcon,
+    InfoIcon,
+    InstagramIcon,
+    LinkedInIcon,
+    MenuIcon,
+    NotificationIcon,
+    PlusBIcon,
+    RightDarkBIcon,
+    RightDarkBlueIcon,
+    SearchIcon,
+    ShortIcon,
+    StarOIcon,
+    SuccessfulIcon,
+    UserIcon,
+    ImageOne,
+    ImageTwo,
+    ImageThree,
+    ImageFour,
+    ImageFive,
+    ImageSix,
+    ImageSeven,
+    ImageEight,
+    ImageNine,
+    ImageTen,
+    ImageEleven,
+    ProfilePictureOne,
+    ProfilePictureTwo,
+    ProfilePictureThree,
+    ProfilePictureFour,
+    ProfilePictureFive,
+    ProfilePictureSix,
+    ProfilePictureSeven,
+    ProfilePictureEight,
+    Cameraman,
+    CameraProfile,
+    ImgLp,
+    ImageTwelve,
+    CameraMan,
+    Producation,
+    HandCamera,
+    location,
+    MemberCameraman,
+    Actor,
+    Travel,
+    Singer,
+    CrewMembers,
+    ServiceProvider,
+    naomi,
+    van,
+    van1,
+}
